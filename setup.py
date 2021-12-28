bca736c5b6c0425089271f9edd34242d7ffc0563
Na-He <81353915+Na-He@users.noreply.github.com>
2021-12-28 09:58:29 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='NRFinder',
+    version='1.0',
+    description='Fast-NR is a software which applied on STARR-seq data to identify the negative regulatory elements, like silencer or insulator.',
+    long_description=readme,
+    author='Na He',
+    author_email='11849492@mail.sustech.educ.com',
+    url='https://github.com/Na-He/Fast-NR',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    entry_points = {
+          'console_scripts': [
+              'Fast-NR = src.Fast-NR:main'
+          ]
+          }
+)
+© 2021 GitHub, Inc.
+Terms
