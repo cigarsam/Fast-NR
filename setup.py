5c5f96e84c2b6a9f58d360b3a024e3cc4bbb0df7
Na-He <81353915+Na-He@users.noreply.github.com>
2021-12-28 16:58:29 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,18 +8,18 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='FastNR',
+    name='Fast-NR',
     version='1.0',
-    description='FastNR is a software which applied on STARR-seq data to identify the negative regulatory elements, like silencer or insulator.',
+    description='Fast-NR is a software which applied on STARR-seq data to identify the negative regulatory elements, like silencer or insulator.',
     long_description=readme,
     author='Na He',
     author_email='11849492@mail.sustech.educ.com',
-    url='https://github.com/Na-He/FastNR',
+    url='https://github.com/Na-He/Fast-NR',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     entry_points = {
           'console_scripts': [
-              'FastNR = src.FastNR:main'
+              'Fast-NR = src.FastNR:main'
           ]
           }
 )
